fa8af81d2d9712d996e07058449c9105650a1c23
Jordan B <jikao55@outlook.fr>
2024-05-18 16:35:19 +0200

    test

--- commands/Utili/user.js
+++ commands/Utili/user.js
@@ -3,10 +3,10 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("user")
-    .setDescription("Provides information about the user."),
+    .setDescription("Information sur l'utilisateur"),
   async execute(interaction) {
     await interaction.reply(
-      `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
+      `Cette commande à été faite par ${interaction.user.username}, qui à rejoint le ${interaction.member.joinedAt}.`
     );
   },
 };
